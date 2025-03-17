47026a0bf6d35fb0b91b0d40d8e00bb7a9cd8a1e
Luisa Fernanda Henao Posada <tuemail@example.com>
2025-03-16 21:11:18 -0500

    Resolviendo conflictos

--- Desktop/POO/SistemaBiblioteca/src/autonoma/sistemabiblioteca/app/main/SistemaBiblioteca.java
+++ Desktop/POO/SistemaBiblioteca/src/autonoma/sistemabiblioteca/app/main/SistemaBiblioteca.java
@@ -1,20 +1,16 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package autonoma.sistemabiblioteca.app.main;
 
 /**
- *
  * @author Luisa Fernanda Henao
+ * @since 20251603
+ * @version 1.0.0
  */
 public class SistemaBiblioteca {
 
-    /**
-     * @param args the command line arguments
-     */
+  
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
