c134b564310df3ead6f7d96b1b0e698f4cdad40e
Kallst <santiago.castrom@autonoma.edu.co>
2025-03-16 21:02:41 -0500

    Se eliminaron archivos del proyecto viejo y se reemplazaron por archivos nuevos. Además, se inició con la interfaz gráfica

---
+++ Desktop/POO/SistemaBiblioteca/src/main/java/autonoma/sistemabiblioteca/app/SistemaBiblioteca.java
@@ -0,0 +1,14 @@
+package autonoma.sistemabiblioteca.app;
+
+/**
+ *
+ * @author Sofia Bermudez
+ * @since 20251603
+ * @version 1.0.0
+ */
+public class SistemaBiblioteca {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
